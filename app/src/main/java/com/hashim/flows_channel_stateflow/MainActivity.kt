b811d17d59b292ebaffba27f406425971b4786de
hashim <hashimtahir@live.com>
2021-03-10 16:12:11 +0500

    Initial Commit

---
+++ app/src/main/java/com/hashim/flows_channel_stateflow/MainActivity.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021/  3/ 10.  Created by Hashim Tahir
+ */
+
+package com.hashim.flows_channel_stateflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
